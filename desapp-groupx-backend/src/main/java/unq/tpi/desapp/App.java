e0d506801693d715e78f4d4554a6e45d7a7dff99
jeretome <jeremias.tome@live.com.ar>
2018-03-18 17:23:07 -0300

    Maven proyect created

---
+++ desapp-groupx-backend/src/main/java/unq/tpi/desapp/App.java
@@ -0,0 +1,13 @@
+package unq.tpi.desapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
